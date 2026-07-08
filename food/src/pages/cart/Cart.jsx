18db7a6257370feb50ee7f6890fd0cb81a686a9a
Pushpendra Singh Jat <link4ganpatjat@gmail.com>
2026-07-09 00:33:35 +0530

    completed backend for adding items to cart, also used middleware to protect routes for adding items to cart

--- food/src/pages/cart/Cart.jsx
+++ food/src/pages/cart/Cart.jsx
@@ -29,7 +29,7 @@ const Cart = () => {
             return (
               <div key={item._id ?? index}>
                 <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
